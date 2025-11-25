d800c401caabb3980494c40ff9f36f5e3df9e1e7
Mark <mark.mazur007@gmail.com>
2025-11-25 21:59:32 +0200

    Add all necessary math operation to Tensor

--- core/tensor.py
+++ core/tensor.py
@@ -1,5 +1,6 @@
 from typing import Tuple, Optional, List
 import numpy as np
+import math
 
 
 # Base Tensor class defines as a core operational element represented
@@ -12,7 +13,7 @@ class Tensor:
         data: float | List[float],
         _children: Optional[Tuple['Tensor', ...]] | None = (),
         _op: str | None = '',
-        label: str = 'Value'
+        label: str = 'Tensor'
         ) -> None:
         self.data = np.array(data) # Create a numpy array as Tensor representation
         self.shape = self.data.shape # Get the shape from numpy .shape
@@ -23,19 +24,20 @@ class Tensor:
         self.label = label # The name of a Tensor (debug)
 
 # ======== Math operations ========
+    # ADDITION
     def __add__(self, other):
         # Checks if other Tensor is numpy array or other data with the same shape
         other = other if isinstance(other, Tensor) else Tensor(other)
         assert self.shape == other.shape, f'Shapes have to be equal, but got: {self.shape} and {other.shape}'
 
         # Implement addition and return as new Tensor
-        out: Tensor = Tensor(
+        out = Tensor(
             data=self.data + other.data,
             _children=(self, other),
-            _op='AdditionBackward'
+            _op='AddBackward'
         )
 
-        # The derivative of plus operation propagates to all Tensors
+        # Derivative of addition operation propagates to all parent Tensors that form out Tensor
         def _backward():
             self.grad += 1.0 * out.grad
             other.grad += 1.0 * out.grad
@@ -47,6 +49,121 @@ class Tensor:
     def __radd__(self, other):
         return self + other
 
+    # SUBTRACTION
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __rsub__(self, other):
+        pass
+
+    # MULTIPLICATION
+    def __mul__(self, other):
+        # Checks if other Tensor is numpy array or other data with the same shape
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        assert self.shape == other.shape, f'Shapes have to be equal, but got 2 tensors: {self.shape} and {other.shape}'
+
+        out = Tensor(
+            data=self.data * other.data,
+            _children=(self, other),
+            _op='MulBackward'
+        )
+
+        # Derivative of muliplication
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def __rmul__(self, other):
+        return self * other
+
+    # MATRIX MULTIPLICATION
+    def __matmul__(self, other):
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        assert self.shape[-1] == other.shape[0], f'Shapes have to be equal in N dim ((M, N) and (N, K)), got 2 tensors: {self.shape} and {other.shape}'
+
+        out = Tensor(
+            data=self.data @ other.data,
+            _children=(self, other),
+            _op='MatMulBackward'
+        )
+
+        def _backward():
+            self.grad += out.grad @ other.data.T
+            other.grad += self.data.T @ out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def __rmatmul__(self, other):
+        return other @ self
+
+    # DIVISION
+    def __truediv__(self, other):
+        return self * other**-1
+
+    # EXPONENTIAL
+    def __pow__(self, other):
+        # Checks if other is scaler
+        assert isinstance(other, (int, float)), '__pow__ works only with scalers'
+
+        out = Tensor(
+            data=self.data ** other,
+            _children=(self,),
+            _op='PowBackward'
+        )
+
+        # Derivative of exponent
+        def _backward():
+            self.grad += (other * (self.data ** (other - 1))) * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def __rpow__(self, other):
+        # Checks if other is scaler
+        assert isinstance(other, (int, float)), f'Base must to be a scaler'
+
+        out = Tensor(
+            data=other ** self.data,
+            _children=(self,),
+            _op='RevPowBackward'
+        )
+
+        def _backward():
+            self.grad += (out.data * math.log(other)) * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    # NEGATION
+    def __neg__(self):
+        return self.data * -1
+
 # ======== Representation of the object ========
     def __repr__(self) -> str:
         return f'{self.label}:\n{self.data}\nShape: {self.shape}'
+
+
+x = Tensor([[1., 2.],
+            [3., 4.]])
+
+y = Tensor([[1., 2.],
+            [3., 4.]])
+
+v = Tensor([[-1],
+            [-2]])
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x @ y)
+print(x @ v)
+print(x ** 3)
+print(2 ** v)
